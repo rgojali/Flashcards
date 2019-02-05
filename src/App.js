02c16e8306a8853872a0d3604a4c31fb17f20b7e
Rusilah Gojali <rgojali@gmail.com>
2019-02-05 14:36:07 -0800

    created files and folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './Card/Card';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      cards: [
+        {id: 1, eng: "English", han: "Hanzi", pin: "Pinyin"},
+        {id: 2, eng: "English_2", han: "Hanzi_2", pin: "Pinyin_2"},
+      ],
+      currentCard: {}
+    }
+  }
+
+  componentWillMount(){
+    const currentCards =this.state.cards;
+
+    this.setState({
+      cards: currentCards,
+      currentCard: this.getRandomCard(currentCards)
+    })
+  }
+
+  getRandomCard(currentCards){
+    var card = currentCards[Math.floor(Math.random() * currentCards.length)]
+    return(card);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Card eng={this.state.currentCard.eng} 
+              han={this.state.currentCard.han}
+              pin={this.state.currentCard.pin}
+              />
       </div>
     );
   }
