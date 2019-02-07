3d807deeefe476ced38c082723c6c18dd737f39c
Rusilah Gojali <rgojali@gmail.com>
2019-02-06 23:49:07 -0800

    created files and changed some data from previous files

---
+++ src/Config/Firebase/db_config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyDNfhI0kVCdS6qwy1aKaHLTeoDo0fLyQms",
+    authDomain: "react-flashcards-983ba.firebaseapp.com",
+    databaseURL: "https://react-flashcards-983ba.firebaseio.com",
+    projectId: "react-flashcards-983ba",
+    storageBucket: "react-flashcards-983ba.appspot.com",
+    messagingSenderId: "199021596669"
+}
